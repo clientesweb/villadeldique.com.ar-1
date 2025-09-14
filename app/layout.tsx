ac6ba41858ba76cb5d452909c3065f34f6246096
clientesweb <alexisifran32@gmail.com>
2025-09-14 20:28:53 -0300

    Actualizar layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: "your-google-verification-code", // Placeholder for Google verification code
+    google: "google-site-verification=l43oagagLLjGqoWzzMuQvJ3qFwcF3j3Pu7VMo4MPXk4", // Placeholder for Google verification code
   },
   generator: "Next.js",
 }
